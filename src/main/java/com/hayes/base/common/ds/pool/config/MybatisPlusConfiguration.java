399778f5e3fd02c51d60292ac51e7f7fba25c4e6
Mr.HayesLin <lhyzwq@gmail.com>
2022-06-11 23:08:30 +0800

    add MybatisPlusConfiguration

---
+++ src/main/java/com/hayes/base/common/ds/pool/config/MybatisPlusConfiguration.java
@@ -0,0 +1,28 @@
+package com.hayes.base.common.ds.pool.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @program: hayeslin
+ * @Class MybatisPlusConfiguration
+ * @description: 关于此类的描述说明
+ * @author: Mr.HayesLin
+ * @create: 2021-09-12 15:09
+ **/
+@Configuration
+public class MybatisPlusConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
